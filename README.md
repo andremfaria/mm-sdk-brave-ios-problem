116c8fe0c27e89ce7dcfdf761e981f8d7712a10f
andremfaria <andremfaria86@gmail.com>
2024-05-02 16:04:26 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# Starting the server
+``` 
+pnpm install 
+pnpm dev --host
+```
+
+1. Access `https://<your-local-ip>/5174` from the Chrome browser on an Android device.
+2. Check the logs; you will notice that the `.connect()` call never returns.
---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { MetaMaskProvider } from '@metamask/sdk-react'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <MetaMaskProvider 
+    debug={true}
+        sdkOptions={{
+            dappMetadata: {
+              name: "Demo React App",
+              url: window.location.protocol + '//' + window.location.host,
+            }
+        }}>
+
+    <App />
+    </MetaMaskProvider>
+  </React.StrictMode>,
+)
